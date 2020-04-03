a41700a2f752bc022e4d6620388c7febdfa4ec9e
mohsen <maybeevmo@gmail.com>
2020-04-04 00:03:57 +0430

    config file added

---
+++ confing.py
@@ -0,0 +1,24 @@
+chat_id = None # put your own ID here (Integer)
+token = None # your bots toke here (String)
+
+""" Base links for Let Me ... """s
+stack_base_url = "http://lmsotfy.com/?q=" 
+base_url = "https://lmgtfy.com/?q=" 
+
+
+"""Persian messages to send
+Translate to any language if u want to :)
+"""
+start_text = "سلام\nحاجی تون ریک سانچز هستم" 
+pv_text = "شرمنده فقظ تو گروها جواب میدم"
+error_text = "اوه! ارور داد" 
+added_text = "حله اضافه شد" 
+removed_text = "ریموو شد" 
+redire_1 = "\n سوال خود را در اینجا مطرح کنید" 
+redire_2 = "\n"+"سوال شما مربوط به موضوع گروه نیست.سوال خود را در گروه زیر مطرح کنید. \n @pyspy_free" 
+search_help = "اموزش سرچ کردن عبارت" 
+
+""" Others """
+valid_types = ["supergroup","group"] 
+list_args = ["all","list"] 
+
